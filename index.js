4df623248f1c73a14d5db01a10133364d342e932
Loren Bažon <lbazon@unipu.hr>
2025-10-27 17:43:03 +0100

    Samostalni zadatak za vježbu 1

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const PORT = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "about.html"));
+});
+
+const users = [
+  { id: 1, ime: "Ivan", prezime: "Ivić" },
+  { id: 2, ime: "Ana", prezime: "Anić" },
+  { id: 3, ime: "Marija", prezime: "Marić" },
+];
+
+app.get("/users", (req, res) => {
+  res.json(users);
 });
 
 app.listen(PORT, (error) => {
